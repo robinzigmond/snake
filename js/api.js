1edf54f4f1a71d58f1670cbdf5f8432bb1bc1739
Robin Zigmond <robinzig@hotmail.com>
2017-08-06 18:03:01 +0100

    added necessary api.js file, removed .gitignore

---
+++ js/api.js
@@ -0,0 +1,5 @@
+var key = "AIzaSyC2CmphUd-aPE4Bpso2spOnSY8TxHaaS_Q";
+var domain = "snake-game-df93e.firebaseapp.com";
+var databaseURL = "https://snake-game-df93e.firebaseio.com";
+var messengerId = "546734805172";
+var project = "snake-game-df93e";
