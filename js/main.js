6d0aeaa998d68282179e3e7ac93ffae02a92f4a5
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-06 11:13:33 +0100

    Started to add firebase

---
+++ js/main.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2CmphUd-aPE4Bpso2spOnSY8TxHaaS_Q",
+    authDomain: "snake-game-df93e.firebaseapp.com",
+    databaseURL: "https://snake-game-df93e.firebaseio.com",
+    projectId: "snake-game-df93e",
+    storageBucket: "",
+    messagingSenderId: "546734805172"
+  };
+  firebase.initializeApp(config);
+
+  console.log(firebase);
