a9576f66bde2cdec549d89870f86c0ba65c25f77
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-06 12:28:53 +0100

    Added sign in with google

--- js/main.js
+++ js/main.js
@@ -10,3 +10,41 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  var ref = database.ref('scores');
+  var user;
+
+  var data = {
+      name: "DTS",
+      score: 10
+  };
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
+  function signIn() {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        user = result.user;
+        showProfile();
+        
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });  
+
+  }
+
+  function showProfile(){
+    $("#login").hide();
+    $("#profile").show();
+    $("#playername").html(user.displayName)
+  }
